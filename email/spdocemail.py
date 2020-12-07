d4427d14205f372ecf8b7bc73139296aee794c9f
flavioafj <flavioafj@yahoo.com.br>
2020-12-06 21:46:57 -0300

    Conclusão do envio de email

--- email/spdocemail.py
+++ email/spdocemail.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 import pdb
 
+f = open("C:\\Users\\flavi\\OneDrive\\Documentos\\Automação spdoc\\modelo_consulta.txt", "r", encoding="utf-8")
+texto = f.read()
 
 driver = webdriver.Chrome(executable_path=r'chromedriver.exe')
 driver.get("https://webmail.fazenda.sp.gov.br/owa/")
@@ -18,12 +20,14 @@ WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.CSS_SELECTOR,
 driver.find_element(By.CSS_SELECTOR, '[autoid="_fp_7"]').send_keys("flavioafj@yahoo.com.br")
 
 #assunto
-driver.implicitly_wait(3)
+driver.implicitly_wait(5)
 driver.find_element(By.CSS_SELECTOR, '[autoid="_mcp_k"]').send_keys("teste")
 
+#pdb.set_trace()
+
 #corpo do texto
-driver.implicitly_wait(2)
-driver.execute_script('var iframe = document.getElementById("EditorBody");var quadro = iframe.contentWindow.document.getElementById("MicrosoftOWAEditorRegion");var paragrafos = quadro.getElementsByTagName("p"); paragrafos[0].innerHTML = "<u> 2oi </u>";')
+WebDriverWait(driver, timeout=5).until(lambda e: e.find_element(By.CSS_SELECTOR, '#EditorBody'))
+driver.execute_script('var iframe = document.getElementById("EditorBody");var quadro = iframe.contentWindow.document.getElementById("MicrosoftOWAEditorRegion");var paragrafos = quadro.getElementsByTagName("p"); paragrafos[0].innerHTML = "' + texto + '";')
 
 
 
